7c52fa7e344f370d23fc2841fc1ec4e9c59d4023
Rohan Gautam <rohan1gautam@gmail.com>
2019-03-26 11:58:24 +0800

    implemented Basic API (working)

---
+++ backend/accessDbs_Api.py
@@ -0,0 +1,61 @@
+import requests, json
+from pprint import pprint
+
+headers={
+    "identity": "Group6",
+    "token": "a2ad5b59-22ba-4134-9b55-d4761015ac74"
+}
+
+def customerID(username):
+    url='http://api-gateway-dbs-techtrek.ap-southeast-1.elasticbeanstalk.com/customers/{}/'
+    r=requests.get(url.format(username), headers=headers)
+    return json.loads(r.text)
+
+def customerDetails(customerId):
+    url='http://api-gateway-dbs-techtrek.ap-southeast-1.elasticbeanstalk.com/customers/{}/details'
+    r=requests.get(url.format(customerId), headers=headers)
+    return json.loads(r.text)
+
+def transactionDetails(accId, From, To):
+    url= 'http://api-gateway-dbs-techtrek.ap-southeast-1.elasticbeanstalk.com/transactions/{}?from={}&to={}'
+    r=requests.get(url.format(accId, From, To), headers=headers)
+    return json.loads(r.text)
+
+def depositAccList(customerId):
+    url='http://api-gateway-dbs-techtrek.ap-southeast-1.elasticbeanstalk.com/accounts/deposit/{}'
+    r=requests.get(url.format(customerId), headers=headers)
+    return json.loads(r.text)
+
+def depositAccBalance(accID, month, year):
+    url='http://api-gateway-dbs-techtrek.ap-southeast-1.elasticbeanstalk.com/accounts/deposit/{}/balance?month={}&year={}'
+    r=requests.get(url.format(accID, month, year), headers=headers)
+    return json.loads(r.text)
+
+def creditAccList(customerId):
+    url='http://api-gateway-dbs-techtrek.ap-southeast-1.elasticbeanstalk.com/accounts/credit/{}'
+    r=requests.get(url.format(customerId), headers=headers)
+    return json.loads(r.text)
+
+def creditAccBalance(accID):
+    '''accId is different for credit accounts '''
+    url='http://api-gateway-dbs-techtrek.ap-southeast-1.elasticbeanstalk.com/accounts/credit/{}/balance'
+    r=requests.get(url.format(accID), headers=headers)
+    return json.loads(r.text)
+
+def marketingMessages():
+    url='http://api-gateway-dbs-techtrek.ap-southeast-1.elasticbeanstalk.com/marketing'
+    r=requests.get(url, headers=headers)
+    return json.loads(r.text)
+
+def personalMessages(customerId):
+    url='http://api-gateway-dbs-techtrek.ap-southeast-1.elasticbeanstalk.com/message/{}'
+    r=requests.get(url.format(customerId), headers=headers)
+    return json.loads(r.text)
+
+# sample values for testing :
+# username : marytan or limzeyang
+# customerID: 2 or 1
+# accID(deposit): 74 or 10
+# accID(credit): (106,206) or [null]
+
+# pprint(personalMessages(1))
---
+++ backend/flaskAPI.py
@@ -0,0 +1,27 @@
+'''
+pip install flask-restful   #to install this
+python flaskAPI.py          #to run
+'''
+from flask import Flask
+from flask_restful import Api, Resource, reqparse # 
+import accessDbs_Api
+
+app=Flask(__name__)
+api=Api(app)
+
+# class customerID(Resource):
+#     def get(self, username):
+#         return accessDbs_Api.customerID(username), 200
+#     def post(self,username):
+#         pass
+
+class personalMessages(Resource):
+    def get(self, customerId):
+        print(customerId, type((customerId)))
+        return accessDbs_Api.personalMessages(customerId), 200
+    def post(self, username):
+        pass
+
+
+api.add_resource(personalMessages, "/msg/<int:customerId>")
+app.run(debug=True)
